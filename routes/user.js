8850933b6a78e5ecb43cb0389af3dde0c08e428d
Aaron Corder <agcorder91@gmail.com>
2018-03-22 21:48:00 -0400

    Added ability to change displayname, email, username, password. Changed nav items

--- routes/user.js
+++ routes/user.js
@@ -25,6 +25,22 @@ router.put("/:username", function(req, res) {
       res.redirect("/user/" + req.params.username);
     }
   });
-})
+});
+
+router.put("/:username/pass", function(req, res) {
+  User.findByUsername(req.params.username, function(err, user) {
+    if(req.body.newPassword === req.body.confNewPass) {
+      user.changePassword(req.body.curPassword, req.body.newPassword, function(err) {
+        if(err) {
+          console.log(err);
+          res.redirect("/user/" + req.params.username);
+        }
+        res.redirect("/user/" + req.params.username);
+      });
+    } else {
+      res.redirect("/user/" + req.params.username);
+    }
+  });
+});
 
 module.exports = router;
